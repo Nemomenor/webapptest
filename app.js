2c19d52f1c5978119aa2e498a438ac28ce864f64
Nemomenor <akivisivan@gmail.com>
2023-09-20 13:29:48 +0300

    commit

--- app.js
+++ app.js
@@ -2,59 +2,20 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let item = "";
-
-let start = document.getElementById("start");
 let fail = document.getElementById("failButton");
 let ok = document.getElementById("okButton");
 
-start.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы нажали старт");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
+
 fail.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("FAIL SEND");
-		item = "1";
-		tg.MainButton.show();
-	}
+	let event = "fail";
+	tg.sendData(event)
+	tg.close;
 });
 ok.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("OK SEND");
-		item = "1";
-		tg.MainButton.show();
-	}
+	let event = "ok";
+	tg.sendData(event)
+	tg.close;
 });
-
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
-
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.id}`;
-
-usercard.appendChild(p);
 
 
 
