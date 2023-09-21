9cf8d653985c250e93453a13b936b4fba9b4c288
Nemomenor <akivisivan@gmail.com>
2023-09-21 20:25:15 +0300

    commit

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 p.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.id}
+${tg.initDataUnsafe.user.language_code}
 ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
