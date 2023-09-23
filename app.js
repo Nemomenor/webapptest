328d1d56536208f54e1c170fcae7c6aa48753c59
Nemomenor <akivisivan@gmail.com>
2023-09-23 16:43:01 +0300

    commit

--- app.js
+++ app.js
@@ -4,27 +4,20 @@ tg.expand();
 
 let fail_send = document.getElementById("fail_send");
 let ok_send = document.getElementById("ok_send");
-Telegram.WebApp.ready();
 tg.MainButton.show();
-tg.MainButton.setParams({
-	text: 'CLOSE WEBVIEW',
-	is_visible: true
-}).onClick(tg.close);
+tg.MainButton.setText("FUAAAAAk");
+Telegram.WebApp.onEvent('mainButtonClicked', tg.close())
 
 fail_send.addEventListener("click", function(){
-	let result = Telegram.WebApp.showConfirm("Отправить ❌ FAIL?");
-	if (result){
 		let event = "fail_send";
-		tg.sendData(event)
-		tg.close;
-	}
+		tg.sendData(event);
+		tg.close();
 });
 ok_send.addEventListener("click", function(){
-	//let result = Telegram.WebApp.showConfirm("Отправить ✅ OK?");
 
 		let event = "ok_send";
-		tg.sendData(event)
-		tg.close;
+		tg.sendData(event);
+		tg.close();
 
 
 });
