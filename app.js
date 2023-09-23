e35a1d104e576f52f1fe475862148efe638b44b1
Nemomenor <akivisivan@gmail.com>
2023-09-23 16:24:13 +0300

    commit

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ fail_send.addEventListener("click", function(){
 	}
 });
 ok_send.addEventListener("click", function(){
-	let result = Telegram.WebApp.showConfirm("Отправить ✅ OK?");
-	if (result){
+	//let result = Telegram.WebApp.showConfirm("Отправить ✅ OK?");
+
 		let event = "ok_send";
 		tg.sendData(event)
 		tg.close;
-	}
+
 
 });
 
