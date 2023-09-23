bdee580abb8deef061d2b577dc8acd7fd8de4fe6
Nemomenor <akivisivan@gmail.com>
2023-09-23 23:22:09 +0300

    commit

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-let fail_send = document.getElementById("fail_send");
-let ok_send = document.getElementById("ok_send");
-let custom_send = document.getElementById("custom_send");
-let tickers = document.getElementById("tickers");
-let ticker = tickers.options[tickers.selectedIndex].text;
-let textarea = document.getElementById("textarea").value();
+
 
 tg.MainButton.show();
 tg.MainButton.setParams({
@@ -18,6 +13,12 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
     tg.close();
     //при клике на основную кнопку отправляем данные в строковом виде
 });
+let fail_send = document.getElementById("fail_send");
+let ok_send = document.getElementById("ok_send");
+let custom_send = document.getElementById("custom_send");
+let tickers = document.getElementById("tickers");
+let ticker = tickers.options[tickers.selectedIndex].text;
+let textarea = document.getElementById("textarea").value();
 fail_send.addEventListener("click", function () {
     let result = confirm("отправить FAIL");
     if (result){
