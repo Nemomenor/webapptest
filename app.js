9d90ffde407fdfd33443430b1caf325d22d75095
Nemomenor <akivisivan@gmail.com>
2023-09-24 13:16:34 +0300

    commit

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ tg.expand();
 let fail_send = document.getElementById("fail_send");
 let ok_send = document.getElementById("ok_send");
 let custom_send = document.getElementById("custom_send");
-let tickers = document.getElementById("tickers");
-let ticker = tickers.options[tickers.selectedIndex].text;
-let textarea = document.getElementById("textarea").value;
+
+
 fail_send.addEventListener("click", function () {
     let result = confirm("отправить FAIL");
     if (result){
@@ -17,17 +16,23 @@ fail_send.addEventListener("click", function () {
     }
 });
 ok_send.addEventListener("click", function () {
-    let result = Telegram.WebApp.showConfirm("отправить ок ?");
-    if (result) {
+    let result = confirm("отправить OK");
+    if (result){
         let event = "ok_send";
         tg.sendData(event);
         tg.close();
     }
 });
 custom_send.addEventListener("click", function () {
-    let event = "custom_send";
-        tg.sendData(event+" $ "+ticker+" $ "+ textarea);
+    let result = confirm("отправить CUSTOM");
+    let textarea = document.getElementById("textarea").value;
+    let tickers = document.getElementById("tickers");
+    let ticker = tickers.options[tickers.selectedIndex].text;
+    if (result) {
+        let event = "custom_send";
+        tg.sendData(event + " $ " + ticker + " $ " + textarea);
         tg.close();
+    }
 
 });
 
